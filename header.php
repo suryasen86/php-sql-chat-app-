7262745acba061e07b5e2e251ae849b14dcfe832
tonudon86 <tonudon86@gmail.com>
2021-08-02 21:04:31 +0530

    intial commit

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>noob chat app</title>
+  <link rel="shortcut icon" href="https://img.icons8.com/color/48/000000/chat--v1.png" type="image/x-icon">
 
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
